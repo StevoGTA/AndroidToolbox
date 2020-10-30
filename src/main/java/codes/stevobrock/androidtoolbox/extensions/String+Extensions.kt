38e2721ba6e6087cc6a073f62dec3c0da244becc
Stevo <stevo@sunsetmagicwerks.com>
2020-10-30 11:20:17 -0700

    Added String extensions

---
+++ src/main/java/codes/stevobrock/androidtoolbox/extensions/String+Extensions.kt
@@ -0,0 +1,21 @@
+package codes.stevobrock.androidtoolbox.extensions
+
+//----------------------------------------------------------------------------------------------------------------------
+// String extension
+
+//----------------------------------------------------------------------------------------------------------------------
+val String.asPhoneNumberForDisplay :String get() {
+												// Setup
+												val test = "$this          "
+												val areaCode = test.subSequence(0, 3).filter() { it.isDigit() }
+												val prefix = test.subSequence(3, 6).filter() { it.isDigit() }
+												val suffix = test.subSequence(6, 10).filter() { it.isDigit() }
+
+												// Check results
+												return when {
+													areaCode.isEmpty() ->   ""
+													prefix.isEmpty() ->     "($areaCode"
+													suffix.isEmpty() ->     "($areaCode) $prefix"
+													else ->                 "($areaCode) $prefix-$suffix"
+												}
+											}
